f0a77ce93a8b01fdc21a8047ec708f29bc05b919
Hassan Shehawy <hassan.shehawi@gmail.com>
2018-09-25 17:51:55 +0300

    split the speed data into 2 ranges
    account for the difference in wind speed data as part of it is based on a step of 1 and the other is based on step of 2. Calculation of the part with step of 2 still needs enhancement though

--- weibullsCalc.py
+++ weibullsCalc.py
@@ -24,25 +24,36 @@ for cnt in range(0,2400):
 areas=np.zeros(12)
 addv=0
 for cnt in range(0,11):
-    areas[cnt] = simps(pdfss[(cnt+1)*100:(cnt+3+addv)*100], dx=.01)
+    areas[cnt] = simps(pdfss[(cnt+addv+1)*100:(cnt+3+addv)*100], dx=.01)
     addv=addv+1
-areas[11] = simps(pdfss[2300:24100], dx=.01)*2
+areas[11] = simps(pdfss[2300:2400], dx=.01)*2
      
      
-wbs=np.zeros(23)
-for x in range(3, 26):
-    wbs[x-3]=weibull_min.pdf(x, 2, scale=7.5)
-
+     
+     
+     
+     
+     
+     
+     
+     
 # 3 - 13 step of 1 and 13 - 25 step of 2 
+
+wbs=np.zeros(17)
+for x in range(3, 13):
+    wbs[x-3]=weibull_min.pdf(x, 2, scale=7.5)
+for cnt in range(10, 17):
+    wbs[cnt]=areas[cnt-5]
+
 probs=[wbs[0]]*9
-for cnt in range(1,23):
+for cnt in range(1,17):
     temp=[wbs[cnt]]*9
     for cnt1 in range(0,9):
         probs.append(temp[cnt1])
         
 temp=[0.5,0.25,0.25]
 pwdir=temp
-for cnt in range(0,68):
+for cnt in range(0,50):
     for cnt1 in range(0,3):
         pwdir.append(temp[cnt1])
         
@@ -50,9 +61,9 @@ Nr_speed=[0.3333333]*207
  
 P=[0.975]*207
 
-hours=np.zeros(207)
+hours=np.zeros(153)
 
-for cnt in range(0,207):
+for cnt in range(0,153):
     hours[cnt]=probs[cnt]*pwdir[cnt]*Nr_speed[cnt]*P[cnt]*24*365*20*6
 
 #for cnt in range(0,207):
